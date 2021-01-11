5a70ae4f091f84fd4bb53936d4bc59e1b18ca912
endiawilliams <endiawilliams1@gmail.com>
2021-01-11 17:19:09 -0600

    Nav bar now has JavaScript effects to keep links higlighted while user is scrolling that section.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // highlight the link for the current section 
 
 const containerDiv = document.querySelector('.container')
+const navLinks = document.querySelectorAll('.nav-links')
 
 const home = document.querySelector('.home')
 const aboutMe = document.querySelector('.about-me')
@@ -14,30 +15,76 @@ const skillsLink = document.querySelector('[href="#skills"]')
 const projectsLink = document.querySelector('[href="#projects"]')
 const resumeLink = document.querySelector('[href="#resume"]')
 
-const activeDiv
+let activeDiv
+let lastDiv = 'home'
+
+let scrollListenerActive = true
+
+let lastNavLink
 
 const getActiveDiv = () => {
-    
+    if (aboutMe.getBoundingClientRect().top > 2) {
+        activeDiv = 'home'
+    } else if (aboutMe.getBoundingClientRect().top <= 2 && skills.getBoundingClientRect().top > 2) {
+        activeDiv = 'about-me'
+    } else if (skills.getBoundingClientRect().top <= 2 && projects.getBoundingClientRect().top > 2) {
+        activeDiv = 'skills'
+    } else if (projects.getBoundingClientRect().top <= 2 && resume.getBoundingClientRect().top > 2) {
+        activeDiv = 'projects'
+    } else if (resume.getBoundingClientRect().top <= 2) {
+        activeDiv = 'resume'
+    }
+}
+
+const removeCurrentClass = () => {
+    homeLink.classList.remove('current')
+    aboutMeLink.classList.remove('current')
+    skillsLink.classList.remove('current')
+    projectsLink.classList.remove('current')
+    resumeLink.classList.remove('current')
+}
+
+const assignCurrentClass = () => {
+    switch(activeDiv) {
+        case 'home':
+            homeLink.classList.add('current')
+            break
+        case 'about-me':
+            aboutMeLink.classList.add('current')
+            break
+        case 'skills':
+            skillsLink.classList.add('current')
+            break
+        case 'projects':
+            projectsLink.classList.add('current')
+            break
+        case 'resume':
+            resumeLink.classList.add('current')
+            break
+    }
 }
 
 containerDiv.addEventListener('scroll', () => {
-    if (aboutMe.getBoundingClientRect().top > 0) {
-        homeLink.classList.add('current')
-        aboutMeLink.classList.remove('current')
-    } else if (aboutMe.getBoundingClientRect().top <= 0 && skills.getBoundingClientRect().top > 0) {
-        homeLink.classList.remove('current')
-        aboutMeLink.classList.add('current')
-        skillsLink.classList.remove('current')
-    } else if (skills.getBoundingClientRect().top <= 0 && projects.getBoundingClientRect().top > 0) {
-        aboutMeLink.classList.remove('current')
-        skillsLink.classList.add('current')
-        projectsLink.classList.remove('current')
-    } else if (projects.getBoundingClientRect().top <= 0 && resume.getBoundingClientRect().top > 0) {
-        skillsLink.classList.remove('current')
-        projectsLink.classList.add('current')
-        resumeLink.classList.remove('current')
-    } else if (resume.getBoundingClientRect().top <= 0) {
-        projectsLink.classList.remove('current')
-        resumeLink.classList.add('current')
+    getActiveDiv()
+
+    if (activeDiv == lastNavLink) {
+        scrollListenerActive = true;
     }
+
+    if (!scrollListenerActive) return
+
+    if (activeDiv !== lastDiv) {
+        lastDiv = activeDiv
+        removeCurrentClass()
+        assignCurrentClass()
+    }
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        scrollListenerActive = false
+        removeCurrentClass()
+        e.target.classList.add('current')
+        lastNavLink = e.target.id.slice(0, -4)
+    })
 })
