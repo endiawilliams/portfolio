fd8f626ef5cfa2f0e4e94cb285bc47d063d90cfd
endiawilliams <endiawilliams1@gmail.com>
2021-02-15 06:48:21 -0600

    Adds responsive mobile navbar.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,90 @@
+const navBar = document.querySelector('nav')
+const navHeaders = ['home', 'about-me', 'skills', 'projects', 'contact']
+const navHeadersCap = ['Home', 'About Me', 'Skills', 'Projects', 'Contact']
+
+const addDesktopNav = () => {
+    for (let i = 0; i < navHeaders.length; i++) {
+        const navLink = document.createElement('a')
+        navBar.appendChild(navLink)
+        navLink.setAttribute('href', `#${navHeaders[i]}`)
+        navLink.classList.add('nav-links')
+        navLink.setAttribute('id', `${navHeaders[i]}-nav`)
+        navLink.textContent = `${navHeadersCap[i]}`
+    }
+}
+
+const addMobileNav = () => {
+    const hamburgerMenu = document.createElement('i')
+    navBar.appendChild(hamburgerMenu)
+    hamburgerMenu.classList.add('fas', 'fa-bars', 'fa-2x')
+    hamburgerMenu.setAttribute('onclick', 'openMobileNav()')
+}
+
+const removeNav = () => {
+    while (navBar.firstChild) {
+        navBar.removeChild(navBar.firstChild)
+    }
+}
+
+if (window.innerWidth < 644) {
+    removeNav()
+    addMobileNav()
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 644) {
+        removeNav()
+        addMobileNav()
+        defineNavEls()
+    }
+
+    if (window.innerWidth >= 644) {
+        removeNav()
+        addDesktopNav()
+        defineNavEls()
+
+        navLinks.forEach(link => {
+            link.addEventListener('click', (e) => {
+                console.log(e)
+                scrollListenerActive = false
+                removeCurrentClass()
+                e.target.classList.add('current')
+                lastNavLink = e.target.id.slice(0, -4)
+            })
+        })
+    }
+})
+
+const openMobileNav = () => {
+    document.querySelector('.mobile-nav-menu').style.display = 'block'
+}
+
+const closeMobileNav = () => {
+    document.querySelector('.mobile-nav-menu').style.display = 'none'
+}
+
 // highlight the link for the current section 
-
 const containerDiv = document.querySelector('.container')
-const navLinks = document.querySelectorAll('.nav-links')
 
-const home = document.querySelector('.home')
-const aboutMe = document.querySelector('.about-me')
-const skills = document.querySelector('.skills')
-const projects = document.querySelector('.projects')
-const contact = document.querySelector('.contact')
+let navLinks, home, aboutMe, skills, projects, contact, homeLink, aboutMeLink, skillsLink, projectsLink, contactLink 
 
-const homeLink = document.querySelector('[href="#home"]')
-const aboutMeLink = document.querySelector('[href="#about-me"]')
-const skillsLink = document.querySelector('[href="#skills"]')
-const projectsLink = document.querySelector('[href="#projects"]')
-const contactLink = document.querySelector('[href="#contact"]')
+const defineNavEls = () => {
+    navLinks = document.querySelectorAll('.nav-links')
+
+    home = document.querySelector('.home')
+    aboutMe = document.querySelector('.about-me')
+    skills = document.querySelector('.skills')
+    projects = document.querySelector('.projects')
+    contact = document.querySelector('.contact')
+    
+    homeLink = document.getElementById('home-nav')
+    aboutMeLink = document.getElementById('about-me-nav')
+    skillsLink = document.getElementById('skills-nav')
+    projectsLink = document.getElementById('projects-nav')
+    contactLink = document.getElementById('contact-nav')
+}
+
+defineNavEls()
 
 let activeDiv
 let lastDiv = 'home'
@@ -37,6 +108,8 @@ const getActiveDiv = () => {
 }
 
 const removeCurrentClass = () => {
+    console.log("removeCurrentClass is being called")
+    console.log(homeLink)
     homeLink.classList.remove('current')
     aboutMeLink.classList.remove('current')
     skillsLink.classList.remove('current')
@@ -82,6 +155,7 @@ containerDiv.addEventListener('scroll', () => {
 
 navLinks.forEach(link => {
     link.addEventListener('click', (e) => {
+        console.log(e)
         scrollListenerActive = false
         removeCurrentClass()
         e.target.classList.add('current')
